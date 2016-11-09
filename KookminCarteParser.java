e377a163869ec360cfcc1ab1a2e0d5e510d1fd2d
S. J. Lee <sj9401h@naver.com>
2016-11-10 08:33:10 +0900

    ver 0.1.0
    * 교직원식당 추가
    * 청향 추가
    * jsoup 연결하는 공통부분 메서드로 처리해 객체지향성 확보

--- KookminCarteParser.java
+++ KookminCarteParser.java
@@ -6,31 +6,38 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.util.Calendar;
 
-public class KookminCarteParserJsoup {
+public class KookminCarteParser {
 	private static final String address = "http://kmucoop.kookmin.ac.kr/restaurant/restaurant.php?w=";
 	private static int i;
 	
 	public static void main(String[] args) throws Exception {
 		String today = findToday();
-		bubsikPrint(today);
-		System.out.println();
-		haksikPrint(today);
+		bubsikPrint(today);	System.out.println();
+		haksikPrint(today);	System.out.println();
+		facultyPrint(today);System.out.println();
+		chunghyangPrint(today);
 		System.out.print("\n종료하려면 아무키나 누르세요...");
 		System.in.read();
 	}
-	
+	//오늘의 날짜 찾기
 	private static String findToday(){
 		Calendar cal = Calendar.getInstance();
 		int year = cal.get(Calendar.YEAR), month = cal.get(Calendar.MONTH)+1, date = cal.get(Calendar.DATE);
 		return year+"년 "+month+"월 "+date+"일";
 	}
+	//각 식당마다 오늘 메뉴 부분만 뽑아서 Elements 타입으로 리턴
+	private static Elements jsoupConnect(String address) throws Exception {
+		String tag = "td[bgcolor=#eaffd9]";
+		Document doc = Jsoup.connect(address).get();
+		return doc.select(tag);
+	}
+	//법식 출력
 	private static void bubsikPrint(String today) throws Exception {
 		System.out.println("## 법학관 한울식당 메뉴 ("+today+") ##\n");
 		String tmp; i = 0;
 		String bubsikMenu[] = { "바로바로1", "바로바로2", "면이랑", "밥이랑 하나", "밥이랑 두울", "石火랑", "石火랑(조식)"};
 
-		Document doc = Jsoup.connect(address+1).get();
-		Elements menu = doc.select("td[bgcolor=#eaffd9]");
+		Elements menu = jsoupConnect(address+1);
 		
 		for (Element res : menu) {
 			tmp = res.text();
@@ -41,19 +48,19 @@ public class KookminCarteParserJsoup {
 			i++;
 		}
 	}
+	//학식 출력
 	private static void haksikPrint(String today) throws Exception {
 		System.out.println("## 복지관 학생식당 메뉴 ("+today+") ##\n");
 		String haksikMenu[] = { "착한아침", "가마", "누들송(면)", "누들송(카페테리아)", "인터쉐프", "데일리밥"};
 		
 		for(i=0;i<6;i++) haksikMenu[i] = "------- <"+haksikMenu[i]+"> -------\n"; i=0;
 		
-		Document doc = Jsoup.connect(address+2).get();
-		Elements menu = doc.select("td[bgcolor=#eaffd9]");
+		Elements menu = jsoupConnect(address+2);
 		
 		for (Element res : menu) {
 			String tmp = res.text();
 			
-			if(i==0) tmp = "*조식*"+tmp+"\n";
+			if(i==0) tmp = "*조식* "+tmp+"\n";
 			else if(i<6) tmp = "*중식* "+tmp+"\n";
 			else tmp = "*석식* "+tmp+"\n";
 			
@@ -65,5 +72,34 @@ public class KookminCarteParserJsoup {
 		}
 		
 		for(i=0;i<6;i++) System.out.println(haksikMenu[i]);
+	}
+	//교직원식당 출력
+	private static void facultyPrint(String today) throws Exception{
+		System.out.println("## 복지관 교직원식당 메뉴 ("+today+") ##\n");
+		String facultyMenu[] = {"키친1", "키친2", "주문식", "석식"};
+		
+		for(i=0;i<4;i++) facultyMenu[i] = "------- <"+facultyMenu[i]+"> -------\n"; i=0;
+		
+		Elements menu = jsoupConnect(address+3);
+		
+		for(Element res : menu){
+			String tmp = res.text();
+			
+			if(i>2) tmp = "*석식* "+tmp+"\n";
+			else tmp = "*중식* "+tmp+"\n";
+			
+			System.out.println(facultyMenu[i++]+=tmp);
+		}
+	}
+	//청향 출력
+	private static void chunghyangPrint(String today) throws Exception{
+		System.out.println("## 법학관 청향 메뉴 ("+today+") ##\n");
+		String chunghyangMenu[] = {"메뉴1", "메뉴2", "메뉴3", "메뉴4", "메뉴5", "메뉴6", "메뉴7"};
+		
+		for(i=0;i<7;i++) chunghyangMenu[i] = "------- <"+chunghyangMenu[i]+"> -------\n"; i=0;
+		
+		Elements menu = jsoupConnect(address+4);
+		
+		for(Element res : menu) System.out.println(chunghyangMenu[i++]+"*중식* "+res.text()+"\n");
 	}
 }
