db95fc7201e5663a572cd1d5d221f3bf15644235
S. J. Lee <sj9401h@naver.com>
2016-11-28 09:52:17 +0900

    ver 0.2.4
    * 법식메뉴 중 중식과 학식이 분리되지 않는 버그 수정

--- KookminCarteParser.java
+++ KookminCarteParser.java
@@ -38,62 +38,70 @@ public class KookminCarteParser {
 	
 	//법식
 	private static void parseBubsik() throws Exception {
-        	String bubsik = "[법식]", tmp;
-        	Elements menu = jsoupConnect(address+1);
-        	for (Element res : menu) {
-            		tmp = removeBracket(res.text());
-            		if(tmp.contains("*중식*"))lunch+=bubsik+tmp.substring(tmp.indexOf("*중식*")+4)+"\n";
-            		else if(tmp.contains("*석식*")) dinner+=bubsik+tmp.substring(tmp.indexOf("*석식*")+4)+"\n";
-            		else if(tmp.contains("*중석식*")){
-                		lunch+=bubsik+tmp.substring(tmp.indexOf("*중석식*")+5)+"\n";
-                		dinner+=bubsik+tmp.substring(tmp.indexOf("*중석식*")+5)+"\n";
-            		}
-           		else breakfast+=bubsik+" "+tmp+"\n"; //조식
-        	}
-        	breakfast+="\n"; lunch+="\n"; dinner+="\n";
-	}
+        String bubsik = "[법식]", tmp;
+        Elements menu = jsoupConnect(address+1);
+        for (Element res : menu) {
+            tmp = removeBracket(res.text());
+            if(tmp.contains("*중식*")){ //중식으로 시작하는 경우
+            	if(tmp.contains("*석식*")){ //중식과 석식이 한줄에 같이 있는 경우
+            		lunch+=bubsik+tmp.substring(tmp.indexOf("*중식*")+4, tmp.indexOf("*석식*"))+"\n";
+            		dinner+=bubsik+tmp.substring(tmp.indexOf("*석식*")+4)+"\n";
+            	}
+            	else lunch+=bubsik+tmp.substring(tmp.indexOf("*중식*")+4)+"\n"; //중식만 있는 경우
+            }
+            //석식만 있는 경우
+            else if(tmp.contains("*석식*")) dinner+=bubsik+tmp.substring(tmp.indexOf("*석식*")+4)+"\n";
+            //중석식 형태로 있는 경우
+            else if(tmp.contains("*중석식*")){
+                lunch+=bubsik+tmp.substring(tmp.indexOf("*중석식*")+5)+"\n";
+                dinner+=bubsik+tmp.substring(tmp.indexOf("*중석식*")+5)+"\n";
+            }
+            else breakfast+=bubsik+" "+tmp+"\n"; //조식
+        }
+        breakfast+="\n"; lunch+="\n"; dinner+="\n";
+    }
 	
 	//학식
-    	private static void parseHaksik() throws Exception {
-        	String haksik = "[학식]", tmp;
-        	Elements menu = jsoupConnect(address+2);
-        	int i=0;
-        	for (Element res : menu) {
-            		tmp = removeBracket(res.text());
-            		if(i==0) breakfast+=(haksik+" "+tmp+"\n"); //조식
-            		else if(i<6) lunch+=(haksik+" "+tmp+"\n"); //중식
-            		else dinner+=(haksik+" "+tmp+"\n"); //석식
-            		i++;
-            		if(i>8) break; //학식 중국집 메뉴는 출력 안한다.
-        	}
-        	breakfast+="\n"; lunch+="\n"; dinner+="\n"
-	}
+    private static void parseHaksik() throws Exception {
+        String haksik = "[학식]", tmp;
+        Elements menu = jsoupConnect(address+2);
+        int i=0;
+        for (Element res : menu) {
+            tmp = removeBracket(res.text());
+            if(i==0) breakfast+=(haksik+" "+tmp+"\n"); //조식
+            else if(i<6) lunch+=(haksik+" "+tmp+"\n"); //중식
+            else dinner+=(haksik+" "+tmp+"\n"); //석식
+            i++;
+            if(i>8) break; //학식 중국집 메뉴는 출력 안한다.
+        }
+        breakfast+="\n"; lunch+="\n"; dinner+="\n";
+    }
 	
-  	//교직원식당
-    	private static void parseFaculty() throws Exception{
-        	String faculty = "[교직원]", tmp;
-        	Elements menu = jsoupConnect(address+3);
-        	int i=0;
-		for(Element res : menu){
-		    tmp = removeBracket(res.text());tmp = tmp.substring(tmp.indexOf(']')+1).trim();
-		    if(i>2) dinner+=(faculty+" "+tmp+"\n");//석식
-		    else lunch+=(faculty+" "+tmp+"\n");//중식
-		    i++;
-		}
-		lunch+="\n"; dinner+="\n";
-	}
+  //교직원식당
+    private static void parseFaculty() throws Exception{
+        String faculty = "[교직원]", tmp;
+        Elements menu = jsoupConnect(address+3);
+        int i=0;
+        for(Element res : menu){
+            tmp = removeBracket(res.text());tmp = tmp.substring(tmp.indexOf(']')+1).trim();
+            if(i>2) dinner+=(faculty+" "+tmp+"\n");//석식
+            else lunch+=(faculty+" "+tmp+"\n");//중식
+            i++;
+        }
+        lunch+="\n"; dinner+="\n";
+    }
 	
-	//청향
-	private static void parseChunghyang() throws Exception{
-		String chunghyang = "[청향]", tmp;
-		Elements menu = jsoupConnect(address+4);
-		//청향은 중식만 운영
-		for(Element res : menu){
-		    tmp = removeBracket(res.text());
-		    lunch+=(chunghyang+" "+tmp+"\n");
-		}
-		lunch+="\n";
-	}
+  //청향
+    private static void parseChunghyang() throws Exception{
+        String chunghyang = "[청향]", tmp;
+        Elements menu = jsoupConnect(address+4);
+        //청향은 중식만 운영
+        for(Element res : menu){
+            tmp = removeBracket(res.text());
+            lunch+=(chunghyang+" "+tmp+"\n");
+        }
+        lunch+="\n";
+    }
 	
 	//파싱한 메뉴 출력
 	private static void printAll(){
